74bfd1250261cc9d9ef61fccdaf8552535897092
Yago Aguiar <yagocdj@gmail.com>
2025-04-16 08:20:35 -0300

    feat: composables

--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,4 +1,13 @@
+<script setup lang="ts">
+const { diasPassados, diferencaEmDias } = useFormatacao()
+
+const dataVideo = new Date(2025, 3, 2);
+
+diasPassados(dataVideo);
+</script>
+
 <template>
   <h1>Components - Nuxt 3</h1>
   <p>Autor: Pablo Codes</p>
+  <p>{{ diferencaEmDias }} dias atrás</p>
 </template>
---
+++ composables/useFormatacao.ts
@@ -0,0 +1,15 @@
+export default function () {
+  const diferencaEmDias = ref(0);
+
+  const diasPassados = (data: Date) => {
+    // debugger
+    const dataAtual = new Date().getTime();
+    const diferencaEmMilissegundos = dataAtual - data.getTime();
+
+    diferencaEmDias.value = Math.floor(
+      diferencaEmMilissegundos / (1000 * 60 * 60 * 24)
+    );
+  };
+
+  return { diasPassados, diferencaEmDias };
+}
